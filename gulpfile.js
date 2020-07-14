10d02f6caef38f9875033b063ae9a97f811220fc
Dastan Rakhmanzhanov <dastanrahmanzhanov0017@gmail.com>
2020-07-14 09:37:53 +0600

    New gulpfile.js file created that allows us to: compress imgs, compile sass files into folder CSS into simple css files

---
+++ gulpfile.js
@@ -0,0 +1,32 @@
+const gulp = require("gulp");
+const sass = require("gulp-sass");
+const sourcemaps = require("gulp-sourcemaps");
+const watch = require("gulp-watch");
+const imagemin = require("gulp-imagemin");
+const del = require("del");
+
+gulp.task("sass-compile", function () {
+  return gulp
+    .src("./sass/**/*scss")
+    .pipe(sourcemaps.init())
+    .pipe(sass().on("error", sass.logError))
+    .pipe(sourcemaps.write("./"))
+    .pipe(gulp.dest("./css/"));
+});
+gulp.task("watch", function () {
+  gulp.watch("./sass/**/*scss", gulp.series("sass-compile"));
+  gulp.watch("./img/**", gulp.series("img-compress"));
+});
+gulp.task("img-compress", function () {
+  return gulp
+    .src("./img/**")
+    .pipe(
+      imagemin({
+        progressive: true,
+      })
+    )
+    .pipe(gulp.dest("./img-not-compressed/"));
+});
+
+// tasks default, it will run watch
+gulp.task("default", gulp.parallel("sass-compile", "img-compress", "watch"));
